2a877dec7bfaf6c7089a43ed98a7ac07311c1b11
hjl <1059045153@qq.com>
2019-04-15 15:17:55 +0800

    first commit

---
+++ HttpEncapsulationKotlin/app/src/main/java/com/cats/self/httpencapsulationkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cats.self.httpencapsulationkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
